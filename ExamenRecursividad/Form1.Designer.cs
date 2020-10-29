ceb5b44ac56abc5cc559f2654d166ef177a5e306
Eduardo Renteria <eduardo_eyj_14@hotmail.com>
2020-10-28 17:53:20 -0700

    Agregar archivos de proyecto.

---
+++ ExamenRecursividad/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ExamenRecursividad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(301, 363);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ejercicio 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "2.- numero aleatoreo convertir a  \r\nbinario, octal y hexadecimal de forma recursi" +
+    "va ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Presionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(386, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(381, 347);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ejercicio 2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Presionar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ExamenRecursividad/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamenRecursividad
+{
+    public partial class Form1 : Form
+    {
+        string bin = "";
+        string oct = "";
+        string hex = "";
+        string ulm = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bin = ""; oct = ""; hex = ""; ulm = "";
+            Random n = new Random();
+            int numeroaleatorio = n.Next(1, 1000);
+            
+            binario(numeroaleatorio);
+            octal(numeroaleatorio);
+            hexadecimal(numeroaleatorio);
+            label2.Text = string.Format("El numero aleatorio es: {0} \nBinario es: {1} \nOctal es: {2} \nHexadecimal es: {3}"
+                          , numeroaleatorio, bin,oct,hex);
+
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ulm = "";
+            Random n = new Random();
+            int numeroaulam = n.Next(1, 100);
+            ulam(numeroaulam);
+            MessageBox.Show(ulm);
+        }
+
+        public void binario(int n) {
+            if (n > 1)
+            {
+                binario(n / 2);
+                bin += (n % 2 == 0) ? "0" : "1";
+            }
+            else
+            {
+                bin += "1";
+            }
+        }
+        public void octal(int n)
+        {
+            if (n > 8)
+            {
+                octal(n / 8);
+                oct += (n % 8).ToString();
+            }
+            else
+            {
+                oct += n;  
+            }
+        }
+        public void hexadecimal (int n)
+        {
+            if(n > 16)
+            {
+                hexadecimal(n / 16);
+                int reciduo = n % 16;
+                hexaTomarLetra(reciduo);
+            }
+            else
+            {
+                hexaTomarLetra(n);
+            }
+        }
+
+        public void hexaTomarLetra( int reciduo)
+        {
+            if (reciduo < 10)
+            {
+                hex += reciduo.ToString();
+            }
+            else
+            {
+                switch (reciduo)
+                {
+                    case 10:
+                        hex += "A";
+                        break;
+                    case 11:
+                        hex += "B";
+                        break;
+                    case 12:
+                        hex += "C";
+                        break;
+                    case 13:
+                        hex += "D";
+                        break;
+                    case 14:
+                        hex += "E";
+                        break;
+                    case 15:
+                        hex += "F";
+                        break;
+                    case 16:
+                        hex += "10";
+                        break;
+                }
+            }
+            
+        }
+
+        public void ulam(int numero)
+        {
+            if( numero > 1) {
+                if( numero  % 2 == 0)
+                {
+                    ulm += numero.ToString() + "; ";
+                    ulam(numero / 2);
+                }
+                else
+                {
+                    ulm += numero.ToString() + "; ";
+                    ulam((numero * 3) + 1 );
+                }
+            }
+            else { ulm += " 1;"; }
+        }
+
+        
+    }
+}
